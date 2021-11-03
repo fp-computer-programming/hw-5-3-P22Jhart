ac9b49d8eb6fcad8798641cb0b112af3a4abfaa3
Jordan Hart <p22jhart@fairfieldprep.org>
2021-11-02 21:56:57 -0400

    hw

---
+++ hw-5-3.py
@@ -0,0 +1,22 @@
+import calendar 
+
+#question 1
+
+calendar.TextCalendar().pryear(2020)
+
+#question 2
+
+calendar.TextCalendar(6).pryear(2020)
+
+#question 3
+calendar.TextCalendar().pryear(2004,9)
+
+
+
+#question 4
+
+
+
+#question 5
+
+print(calendar.isleap)
